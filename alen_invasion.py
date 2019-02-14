a978855ce4d52304789b9718ef4a4622eb686d8a
kirill <kirill.rum@gmail.com>
2019-02-14 15:42:29 +0300

    Update alen_invasion.py

--- alen_invasion.py
+++ alen_invasion.py
@@ -2,16 +2,23 @@ import sys
 
 import pygame
 
+
+
+# noinspection PyUnresolvedReferences
 def run_game():
+    # noinspection PyUnresolvedReferences
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
 
     while True:
         #Отслеживание клавиатуры и мыши
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)
 
         pygame.display.flip()
 
