793057deb7d0f2b8af7753fb6c4145efdc5f9af1
kirill <kirill.rum@gmail.com>
2019-02-15 16:04:44 +0300

    +

--- alen_invasion.py
+++ alen_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settеings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -12,18 +13,13 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.scree_width, ai_settings.scree_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
 
     while True:
-        #Отслеживание клавиатуры и мыши
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitame()
-
+        gf.check_events(ship)
+        ship.update()
+        gf.updete_screen(ai_settings, screen, ship)
         pygame.display.flip()
 
 run_game()
