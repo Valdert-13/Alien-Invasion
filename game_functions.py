7581b5f6847359a4ddfe8e51d2d761dfa63f51df
kirill <kirill.rum@gmail.com>
2019-02-19 15:13:41 +0300

    +

--- game_functions.py
+++ game_functions.py
@@ -44,12 +44,13 @@ def updete_screen (ai_settings, screen, ship, aliens, bullets):
     ship.blitame()
     aliens.draw(screen)
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     """обновляет позиции пули и уничтажает старые пули"""
     bullets.update()
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+        collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 
 
