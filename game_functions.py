bfb71658872a5ac43cb752fe6dec3f2118a64793
kirill <kirill.rum@gmail.com>
2019-02-20 18:12:07 +0300

    The END

--- game_functions.py
+++ game_functions.py
@@ -140,14 +140,15 @@ def update_aliens (ai_settings, screen, stats, sb, ship, aliens, bullets):
         ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
     check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-def ship_hit(ai_settings, stats, screen, sb, ship, aliens, bullets):
-    if stats.ships_left >0:
-        stats.ship_left -= 1
+def ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets):
+    if stats.ships_left > 0:
+        stats.ships_left -= 1
 
         sb.prep_ships()
     else:
         stats.game_active = False
         pygame.mouse.set_visible(True)
+
     aliens.empty()
     bullets.empty()
 
